7593942233590443b38038210d5c4bd1931e0933
BORISAGAR SIDDHARTH <115538682+sidhu020@users.noreply.github.com>
2024-07-10 15:38:27 +0530

    Update 001.py
    001.py was updated by corect code

--- python/assi-1/001.py
+++ python/assi-1/001.py
@@ -1,1 +1,10 @@
-print "cd";
+# assign-1 Program-1
+# 1. Calculate Addition,Subtraction,Multiplication and Division from 2 numbers provided by user input
+num1 = int(input("Enter Number :- "))
+num2 = int(input("Enter Number :- "))
+add = num1+num2
+sub = num1-num2
+mul = num1*num2
+div = num1/num2
+print(type(div))
+print(add, sub, mul, div)
